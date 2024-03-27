1328e5b27690b15248315f3c82a69f7d77f78247
RishabhSingh4872962 <109566428+RishabhSingh4872962@users.noreply.github.com>
2024-03-27 20:50:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -25,6 +25,9 @@ let details=[];
 	 age.textContent="Age";
 	 email.textContent="Email";
 	 phone.textContent="Phone";
+	 age.data-age="age"
+	 phone.data-phone="phone"
+	 email.data-email="email"
 	 age.addEventListener("click",detail)
 	 email.addEventListener("click",detail)
 	 phone.addEventListener("click",detail)
